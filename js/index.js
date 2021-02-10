664dd9b07dbc888fdb9ff80549ad4c2e98153801
houng6023 <77269277+houng6023@users.noreply.github.com>
2021-02-10 17:16:25 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
+var ww = $(window).width()
+console.log(ww)
+
+if( ww >= 769 ) { 
+    $('#nav .depth1 > li').hover(
+    function(){
+        $(this).addClass('on')
+    },
+    function(){
+        $(this).removeClass('on')
+    })
+
+} else { 
+    $('#nav .depth1 > li').on('click',function(){
+        $(this).toggleClass('on')
+        $(this).siblings().removeClass('on')
+    })
+}
+
+
+
+
+
+
 $(".slide-group").slick({
     autoplay: true, // 자동재생
     autoplaySpeed: 3000, // 간격시간
@@ -10,9 +34,61 @@ $('.article:nth-child(1) .playstop').on('click', function () {
     var $ibutton = $(this).find('i')
     if ($ibutton.hasClass('fa-pause')) {
         $('.slide-group').slick('slickPause')
+        // 바로 위 .slick('slickPause')는 정해진거니깐 바꾸면 안돼!
         $ibutton.removeClass('fa-pause').addClass('fa-play')
     } else {
         $('.slide-group').slick('slickPlay')
         $ibutton.removeClass('fa-play').addClass('fa-pause')
+    }
+})
+
+$('#header .open').on('click', function () {
+    $(this).next().css({
+        display: 'block'
+    })
+    $(this).css({
+        display: 'none'
+    })
+    $(this).next().next().css({
+        display: 'block' 
+    })
+})
+$('#header .close').on('click', function () {
+    $(this).prev().css({
+        display: 'none'
+    })
+    $(this).css({
+        display: 'none'
+    })
+    $(this).prev().prev().css({
+        display: 'block'
+    })
+})
+
+// $('#nav .depth1 > li').on('mouseover', function(){
+//     $(this).addClass('on')
+
+// })
+// $('#nav .depth1 > li').on('mouseout', function(){
+//     $(this).removeClass('on')
+
+// })
+
+// $('#nav .depth1 > li').hover(
+// function(){
+//     $(this).addClass('on')
+// },
+// function(){
+//     $(this).removeClass('on')
+// })
+
+
+//네이브 고정하기
+$(window).on('scroll', function(){
+    var cst = $(window).scrollTop()
+    if(cst>=10 && !$('#header').hasClass('on')  ){
+        $('#header').addClass('on')
+    }else if (cst<10 && $('#header').hasClass('on')){
+        $('#header').removeClass('on')
     }
 })
